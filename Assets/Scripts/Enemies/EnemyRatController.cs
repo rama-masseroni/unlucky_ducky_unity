8609545a09b363878999162b7d7c9f35c1225018
Ramiro Masseroni <rama.masseroni@gmail.com>
2026-05-15 00:10:16 -0300

    Rat

---
+++ Assets/Scripts/Enemies/EnemyRatController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemyRatController : GridWalkerController
+{
+    [SerializeField] private bool killsPlayerOnContact = true;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        TryKillPlayer(other);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        TryKillPlayer(other);
+    }
+
+    private void TryKillPlayer(Collider2D other)
+    {
+        if (!killsPlayerOnContact || other == null)
+        {
+            return;
+        }
+
+        PlayerDuckController player = other.GetComponentInParent<PlayerDuckController>();
+
+        if (player == null || player.IsDead)
+        {
+            return;
+        }
+
+        player.Kill();
+    }
+}
